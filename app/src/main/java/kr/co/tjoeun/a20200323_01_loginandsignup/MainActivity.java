a2aa3ac89239f169db7595831d94ca2661a5325a
kws970609 <kws970609@users.noreply.github.com>
2020-03-23 12:53:08 +0900

    연결 실패시 로그 출력

--- app/src/main/java/kr/co/tjoeun/a20200323_01_loginandsignup/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/a20200323_01_loginandsignup/MainActivity.java
@@ -5,14 +5,19 @@ import androidx.databinding.DataBindingUtil;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.CompoundButton;
+
+import org.json.JSONObject;
 
 import java.math.MathContext;
 
 import kr.co.tjoeun.a20200323_01_loginandsignup.databinding.ActivityMainBinding;
 import kr.co.tjoeun.a20200323_01_loginandsignup.utils.ContextUtil;
+import kr.co.tjoeun.a20200323_01_loginandsignup.utils.ServerUtil;
+import okhttp3.FormBody;
 
 public class MainActivity extends BaseActivity {
 
@@ -31,19 +36,23 @@ public class MainActivity extends BaseActivity {
     @Override
     public void setupEvents() {
 
-//        체크 박스에 변화가 있을때 마다 체크 여부를 저장해두려함.
+//        체크박스에 체크가 될때 (변화가 있을때) 마다
+//        체크 여부를 저장.
 
         binding.idCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
 
-//               ContextUtil을 이용해서, 체크여부를 저장.
+//                ContextUtil을 이용해서, 체크 여부를 저장.
                 ContextUtil.setIdCheck(mContext, isChecked);
 
             }
         });
 
-//        로그인 버튼을 누르면 => 입력되어있는 이메일 저장
+
+//        로그인 버튼을 누르면 => 아이디 저장이 체크되어 있다면
+//        => 입력되어있는 이메일 저장
+//        그렇지 않다면 => 이메일을 빈칸 "" 으로 저장
         binding.loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -51,16 +60,30 @@ public class MainActivity extends BaseActivity {
 //                체크박스에 체크가 되어있나?
                 if (binding.idCheckBox.isChecked()) {
 
-//                    체크되어있는 상황
+//                    체크가 되어 있는 상황
                     String inputEmail = binding.emailEdt.getText().toString();
 
                     ContextUtil.setEmail(mContext, inputEmail);
-
                 }
                 else {
-//                    체크 안된 상황
-                    ContextUtil.setEmail(mContext,"");
+//                    체크가 안된 상황
+                    ContextUtil.setEmail(mContext, "");
                 }
+
+
+                String inputEmail = binding.emailEdt.getText().toString();
+                String inputPw = binding.pwEdt.getText().toString();
+
+                ServerUtil.postRequestLogin(mContext, inputEmail, inputPw, new ServerUtil.JsonResponseHandler() {
+                    @Override
+                    public void onResponse(JSONObject json) {
+
+                        Log.d("JSON내용-메인에서", json.toString());
+
+                    }
+                });
+
+
 
             }
         });
--- app/src/main/java/kr/co/tjoeun/a20200323_01_loginandsignup/utils/ServerUtil.java
+++ app/src/main/java/kr/co/tjoeun/a20200323_01_loginandsignup/utils/ServerUtil.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.util.Log;
 
 import org.jetbrains.annotations.NotNull;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
@@ -17,54 +18,76 @@ import okhttp3.Response;
 
 public class ServerUtil {
 
-//    이론
-//    서버 통신 주체? ServerUtil
-//    응답 처리? 액티비티가 함. => 인터페이스로 연결.
+//     이론
+//     서버통신 주체? ServerUtil
+//     응답처리? 액티비티가 함. => 인터페이스로 연결.
 
-    public interface JsonResposeHandler {
+    public interface JsonResponseHandler {
         void onResponse(JSONObject json);
     }
-//    서버 호스트 주소를 편하게 가져다 쓰려고 변수로 저장.
+
+    //    서버 호스트 주소를 편하게 가져다 쓰려고 변수로 저장.
     private static final String BASE_URL = "http://192.168.0.236:5000";
 
-//    로그인 요청 기능 메쏘드
-//    파라미터의 기본구조 : 어떤 화면에서 어떤 응답처리를할지? 변수로.
-//    파라미터 추가: 서버로 전달할때 필요한 데이터들을 변수로.
-    public static void postRequestLogin(Context context, String id, String pw, JsonResposeHandler handler) {
+    //    로그인 요청 기능 메쏘드
+//    파라미터 기본구조 : 어떤화면에서? 어떤응답처리를할지? 변수로.
+//    파라미터 추가 : 서버로 전달할때 필요한 데이터들을 변수로.
+    public static void postRequestLogin(Context context, String id, String pw, final JsonResponseHandler handler) {
+
 //        클라이언트 역할 수행 변수 생성.
         OkHttpClient client = new OkHttpClient();
 
-//        어느 주소로(호스트 주소/기능주소) 갈지? 변수로 저장.
+//        어느주소(호스트주소/기능주소)로 갈지? String 변수로 저장.
 //        192.?.?.?:5000/auth
+        String urlStr = String.format("%s/auth", BASE_URL);
 
-        String urlStr = String.format("%s/auth",BASE_URL);
-
-//        서버로 들고갈 파라미터 담아주기.
-        FormBody fomData = new FormBody.Builder()
+//        서버로 들고갈 파라미터를 담아줘야함.
+        FormBody formData = new FormBody.Builder()
                 .add("login_id", id)
                 .add("password", pw)
                 .build();
 
         Request request = new Request.Builder()
                 .url(urlStr)
-                .post(fomData)
+                .post(formData)
                 .build();
-//                필요한 경우 .header도 추가해야함
+//                필요한 경우 헤더도 추가해야함
 
 
         client.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(@NotNull Call call, @NotNull IOException e) {
-//                연결에 실패했을 경우
+                Log.e("서버 연결 실패","연결안됨");
+//                연결 실패 처리
             }
 
             @Override
             public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-//               연결에 성공했을때=> 그냥 string변수로 뽑아낸다.
+
+//                연결 성공해서 응답이 돌아왔을때 => string()으로 변환.
                 String body = response.body().string();
-                Log.d("로그인 응답",body);
+                Log.d("로그인응답", body);
+
+//                응답 내용을 JSON객체로 가공.
+                try {
+//                    body의 String을 => JSONObject 형태로 변환
+//                    양식에 맞지 않는 내용이면, 앱이 터질 수 있으니
+//                    try / catch 로 감싸도록 처리.
+                    JSONObject json = new JSONObject(body);
+
+//                    이 JSON에 대한 분석은 화면단에 넘겨주자.
+                    if (handler != null) {
+                        handler.onResponse(json);
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
             }
         });
+
+
     }
 
 

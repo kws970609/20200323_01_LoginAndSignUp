f691f10d404a58208ce9661e2f7b04667c3a489b
kws970609 <kws970609@users.noreply.github.com>
2020-03-23 09:50:52 +0900

    로그인 화면 초안

--- app/src/main/java/kr/co/tjoeun/a20200323_01_loginandsignup/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/a20200323_01_loginandsignup/MainActivity.java
@@ -1,14 +1,19 @@
 package kr.co.tjoeun.a20200323_01_loginandsignup;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import kr.co.tjoeun.a20200323_01_loginandsignup.databinding.ActivityMainBinding;
+
+public abstract class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+      binding= DataBindingUtil.setContentView(this,R.layout.activity_main);
     }
 }

f58318bbd43e676bb67f26587607b1c37fde1d2f
kws970609 <kws970609@users.noreply.github.com>
2020-03-23 10:35:52 +0900

    체크박스 체크여부에 따른 저장행동 변경

--- app/src/main/java/kr/co/tjoeun/a20200323_01_loginandsignup/BaseActivity.java
+++ app/src/main/java/kr/co/tjoeun/a20200323_01_loginandsignup/BaseActivity.java
@@ -1,30 +1,16 @@
 package kr.co.tjoeun.a20200323_01_loginandsignup;
 
 import android.content.Context;
-import android.os.Bundle;
-import android.os.PersistableBundle;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.databinding.DataBindingUtil;
-
-import kr.co.tjoeun.a20200320_02_pizzastorelist.databinding.ActivityMainBinding;
 
 public abstract class BaseActivity extends AppCompatActivity {
 
-    ActivityMainBinding binding = null;
 
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
-    }
 
     public Context mContext = this;
 
     public abstract void setupEvents();
-
     public abstract void setValues();
 
 }
--- app/src/main/java/kr/co/tjoeun/a20200323_01_loginandsignup/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/a20200323_01_loginandsignup/MainActivity.java
@@ -4,16 +4,62 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
+
+import java.math.MathContext;
 
 import kr.co.tjoeun.a20200323_01_loginandsignup.databinding.ActivityMainBinding;
+import kr.co.tjoeun.a20200323_01_loginandsignup.utils.ContextUtil;
 
-public abstract class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     ActivityMainBinding binding = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-      binding= DataBindingUtil.setContentView(this,R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        setupEvents();
+        setValues();
+
     }
+
+    @Override
+    public void setupEvents() {
+
+//        로그인 버튼을 누르면 => 입력되어있는 이메일 저장
+        binding.loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+//                체크박스에 체크가 되어있나?
+                if (binding.idCheckBox.isChecked()) {
+
+//                    체크되어있는 상황
+                    String inputEmail = binding.emailEdt.getText().toString();
+
+                    ContextUtil.setEmail(mContext, inputEmail);
+
+                }
+                else {
+//                    체크 안된 상황
+                    ContextUtil.setEmail(mContext,"");
+                }
+
+            }
+        });
+
+    }
+
+    @Override
+    public void setValues() {
+
+//        이 화면을 키면, 저장된 이메일 값을 emailEdt에 입력.
+        binding.emailEdt.setText(ContextUtil.getEmail(mContext));
+
+    }
+
+
+
 }
